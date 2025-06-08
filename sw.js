66248257fa7ce182a2c5a52f6bc04aa61b4a92de
EliteIE <eliteads.ars@gmail.com>
2025-06-07 23:59:00 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -23,7 +23,8 @@ const PRECACHE_ASSETS = [
     '/assets/icons/favicon.ico',
     '/manifest.json',
     'https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&display=swap',
-    'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css'
+    'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css',
+    '/assets/fonts/fontawesome.css'
 ];
 
 // Recursos que não devem ser cacheados
